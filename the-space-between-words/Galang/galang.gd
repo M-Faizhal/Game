370bda13df727a09d40925eb2e4e78d1a6d42305
ersavlrian <esra.valerian@gmail.com>
2024-12-21 18:03:42 +0700

    update

---
+++ the-space-between-words/Galang/galang.gd
@@ -0,0 +1,117 @@
+extends CharacterBody2D
+
+class_name Galang
+
+const speed = 30
+var is_galang_chase: bool
+
+
+var health = 80
+var health_max = 80
+var health_min = 0
+
+var dead: bool = false
+var taking_damage: bool = false
+var damage_to_deal = 20
+var is_dealing_damage: bool = false
+
+var dir: Vector2 = Vector2.ZERO
+const gravity = 900
+var knockback_force = -20
+var is_roaming: bool = false
+
+var player: CharacterBody2D
+var player_in_area = false
+
+func _process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0 
+		
+	if Global.PlayerAlive:
+		is_galang_chase = true
+	elif !Global.PlayerAlive:
+		is_galang_chase = false
+	
+	Global.GalangDamageAmount = damage_to_deal
+	Global.GalangDamageZone = $GalangAttack
+	player = Global.Playerbody
+	
+	move(delta)
+	move_and_slide() 
+	
+	handle_animation()
+
+		
+func move(delta):
+	if !dead:
+		if !is_galang_chase:
+			velocity += dir * speed * delta
+		elif is_galang_chase and !taking_damage:
+			var dir_to_player = position.direction_to(player.position) * speed
+			velocity.x = dir_to_player.x
+			dir.x = abs(velocity.x) / velocity.x
+		elif taking_damage:
+			var knockback_dir = position.direction_to(player.position) * knockback_force
+			velocity.x = knockback_dir.x
+		is_roaming = true
+	elif dead:
+		velocity.x = 0
+
+func handle_animation():
+	var anim_sprite = $AnimatedSprite2D
+	if !dead and !taking_damage and !is_dealing_damage:
+		anim_sprite.play("walk")
+		if dir.x == -1:
+			anim_sprite.flip_h = true
+		elif dir.x == 1:
+			anim_sprite.flip_h = false
+	elif !dead and taking_damage and !is_dealing_damage:
+		anim_sprite.play("hurt")
+		await get_tree().create_timer(0.6).timeout
+		taking_damage = false 
+	elif dead and is_roaming:
+		is_roaming = false
+		anim_sprite.play("death")
+		await get_tree().create_timer(1.0).timeout
+		handle_death()
+	elif !dead and is_dealing_damage:
+		anim_sprite.play("attack")
+		
+func handle_death():
+	self.queue_free()
+
+func _on_direction_timer_timeout():
+	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
+	if !is_galang_chase:
+		dir = choose([Vector2.RIGHT, Vector2.LEFT])
+		velocity.x = 0
+	
+func _ready():
+	$DirectionTimer.start() 
+	
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+
+func _on_galang_hitbox_area_entered(area):
+	var damage = Global.PlayerDamageAmount
+	if area == Global.PlayerDamageZone:
+		take_damage(damage)
+		
+func take_damage(damage):
+	health -= damage
+	taking_damage = true
+	if health <= health_min:
+		health = health_min
+		dead = true
+	print(str(self), "current health is", health)
+
+
+func _on_galang_attack_area_entered(area):
+	if area == Global.PlayerHitbox:
+		is_dealing_damage = true
+		await get_tree().create_timer(1.0).timeout
+		is_dealing_damage = false
--- the-space-between-words/Player/player.gd
+++ the-space-between-words/Player/player.gd
@@ -1,35 +1,101 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var deal_damage_zone = $AttackPlayer
 
 const speed = 150.0
 const jump_power = -350.0
 
+var attack_type: String
+var current_attack: bool
+var siaga: bool
+
 var gravity = 900
+
+var health = 100
+var health_max = 100
+var health_min = 0
+var can_take_damage: bool
+var dead: bool
 
 func _ready():
 	Global.Playerbody = self
+	current_attack = false
+	dead = false
+	can_take_damage = true
+	Global.PlayerAlive = true
 
 func _physics_process(delta):
+	siaga = Global.PlayerSiaga
+	Global.PlayerDamageZone = deal_damage_zone
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power
+		
+	if !dead:
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_power
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		var direction = Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 
+		if siaga and !current_attack:
+			if Input.is_action_just_pressed("left_mouse") or Input.is_action_just_pressed("right_mouse"):
+				current_attack = true
+				if Input.is_action_just_pressed("left_mouse") and is_on_floor():
+					attack_type = "attack1"
+				elif Input.is_action_just_pressed("right_mouse") and is_on_floor():
+					attack_type = "attack2"
+				else:
+					attack_type = "air"
+				set_damage(attack_type)
+				handle_attack_animation(attack_type)
+		handle_movement_animation(direction)
+		check_hitbox()
 	move_and_slide()
-	handle_movement_animation(direction)
+
+func check_hitbox():
+	var hitbox_areas = $PlayerHitbox.get_overlapping_areas()
+	var damage: int
+	if hitbox_areas:
+		var hitbox = hitbox_areas.front()
+		if hitbox.get_parent() is Galang:
+			damage = Global.GalangDamageAmount
+			
+	if can_take_damage:
+		take_damage(damage)
+
+func take_damage(damage):
+	if damage != 0:
+		if health > 0:
+			health -= damage
+			print("player health", health)
+			if health <= 0:
+				health = 0
+				Global.PlayerAlive = false
+				handle_death_animation()
+			take_damage_cooldown(1.0)
+
+func handle_death_animation():
+	$CollisionShape2D.position.y = 5
+	animated_sprite.play("death")
+	await get_tree().create_timer(0.5).timeout
+	$Camera2D.zoom.x = 4
+	$Camera2D.zoom.y = 4
+	await get_tree().create_timer(3.5).timeout
+	self.queue_free()
+
+func take_damage_cooldown(wait_time):
+	can_take_damage = false
+	await get_tree().create_timer(wait_time).timeout
+	can_take_damage = true
 
 func handle_movement_animation(dir):
-	var siaga = Global.PlayerSiaga
 	if !siaga:
 		if is_on_floor():
 			if !velocity:
@@ -40,17 +106,59 @@ func handle_movement_animation(dir):
 		elif !is_on_floor():
 			animated_sprite.play("fall")
 	if siaga:
-		if is_on_floor():
+		if is_on_floor() and !current_attack:
 			if !velocity:
 				animated_sprite.play("siaga")
 			if velocity:
 				animated_sprite.play("run")
 				toggle_flip_sprite(dir)
-		elif !is_on_floor():
+		elif !is_on_floor() and !current_attack:
 			animated_sprite.play("fall")
 
 func toggle_flip_sprite(dir):
 	if dir == 1:
 		animated_sprite.flip_h = false
+		deal_damage_zone.scale.x = 1
 	if dir == -1:
 		animated_sprite.flip_h = true
+		deal_damage_zone.scale.x = -1
+
+func handle_attack_animation(attack_type):
+	if siaga:
+		if current_attack:
+			animated_sprite.play(attack_type)
+			toggle_damage_collisions(attack_type)
+
+func toggle_damage_collisions(attack_type):
+	var damage_zone_collision = deal_damage_zone.get_node("CollisionShape2D")
+	var wait_time: float
+	if attack_type == "air":
+		wait_time = 0.8
+	elif attack_type == "attack1":
+		await get_tree().create_timer(0.3).timeout  
+		damage_zone_collision.disabled = false
+		await get_tree().create_timer(0.2).timeout
+		damage_zone_collision.disabled = true
+		await get_tree().create_timer(0.1).timeout
+		damage_zone_collision.disabled = false
+		await get_tree().create_timer(0.4).timeout
+		damage_zone_collision.disabled = true
+	elif attack_type == "attack2":
+		wait_time = 0.7
+		damage_zone_collision.disabled = false
+		await get_tree().create_timer(wait_time).timeout
+		damage_zone_collision.disabled = true
+
+
+func _on_animated_sprite_2d_animation_finished():
+	current_attack = false
+	
+func set_damage(attack_type):
+	var current_damage_to_deal: int
+	if attack_type == "attack1":
+		current_damage_to_deal = 5
+	elif attack_type == "attack2":
+		current_damage_to_deal = 10
+	elif attack_type == "air":
+		current_damage_to_deal = 7
+	Global.PlayerDamageAmount = current_damage_to_deal
